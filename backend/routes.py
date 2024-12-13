6b9eccdd8c50602d4a4c1fd6cc7ace7cdc93f4fe
Scott Wofford <skawofford@gmail.com>
2024-12-13 15:07:52 -0500

    Finished part 1 of project

--- backend/routes.py
+++ backend/routes.py
@@ -35,16 +35,38 @@ def count():
 ######################################################################
 @app.route("/picture", methods=["GET"])
 def get_pictures():
-    pass
+    if data:
+        return jsonify(data), 200
+    return {"message": "Internal server error"}, 500
 
 ######################################################################
 # GET A PICTURE
 ######################################################################
 
 
+def find_picture(id):
+    global data
+    for picture in data:
+        if picture['id'] == id:
+            return picture
+    return None
+
+def find_picture_id(id):
+    global data
+    for i, picture in enumerate(data):
+        if picture['id'] == id:
+            return i
+    return None
+
 @app.route("/picture/<int:id>", methods=["GET"])
 def get_picture_by_id(id):
-    pass
+    try:
+        picture = find_picture(id)
+        if picture is None:
+            return {"message": "Invalid ID"}, 404
+        return picture, 200
+    except Excteption as e:
+        return {"message": "Internal server error"}, 500
 
 
 ######################################################################
@@ -52,7 +74,15 @@ def get_picture_by_id(id):
 ######################################################################
 @app.route("/picture", methods=["POST"])
 def create_picture():
-    pass
+    try:
+        picture = request.json
+        id = picture['id']
+        if find_picture(id):
+            return {"Message": f"picture with id {id} already present"}, 302
+        data.append(picture)
+        return picture, 201
+    except Exception as e:
+        return {"message": "Internal server error"}, 500
 
 ######################################################################
 # UPDATE A PICTURE
@@ -61,11 +91,30 @@ def create_picture():
 
 @app.route("/picture/<int:id>", methods=["PUT"])
 def update_picture(id):
-    pass
+    try:
+        global data
+        picture = find_picture(id)
+        if picture is None:
+            return {"message": "picture not found"}, 404
+        for key in request.json:
+            picture[key] = request.json[key]
+        return picture, 200
+    except Exception as e:
+        return {"message": f"Internal server error{e}"}, 500
+
 
 ######################################################################
 # DELETE A PICTURE
 ######################################################################
 @app.route("/picture/<int:id>", methods=["DELETE"])
 def delete_picture(id):
-    pass
+    try:
+        global data
+        picture = find_picture(id)
+        if picture is None:
+            return {"message": "picture not found"}, 404
+        data.remove(picture)
+        return {}, 204
+    except Exception as e:
+        return {"message": f"Internal server error{e}"}, 500
+        
